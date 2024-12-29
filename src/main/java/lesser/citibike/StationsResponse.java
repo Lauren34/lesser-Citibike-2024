f08fc7bcc3f53fcaee77a458e057167ed017873c
Lauren Lesser <lesserilana1@gmail.com>
2024-12-29 13:41:25 -0800

    fixed more checkstyles

--- src/main/java/lesser/citibike/StationsResponse.java
+++ src/main/java/lesser/citibike/StationsResponse.java
@@ -6,13 +6,15 @@ public class StationsResponse {
     public Data data;
 
     public static class Data {
-        public Station [] stations;
+        public Station[] stations;
     }
 
     public static class Station {
+        //CHECKSTYLE:OFF
         public String station_id;
         public String name;
         public double lat;
         public double lon;
+        //CHECKSTYLE:ON
     }
 }
--- src/main/java/lesser/citibike/StatusResponse.java
+++ src/main/java/lesser/citibike/StatusResponse.java
@@ -6,12 +6,14 @@ public class StatusResponse {
     public Data data;
 
     public static class Data {
-        public StationStatus [] stations;
+        public StationStatus[] stations;
     }
 
     public static class StationStatus {
+        //CHECKSTYLE:OFF
         public String station_id;
         public int num_bikes_available;
         public int num_docks_available;
+        //CHECKSTYLE:ON
     }
 }
