d7cb446a40953bd91385042e5beb8cf56d4820b1
Lauren Lesser <lesserilana1@gmail.com>
2025-01-06 10:22:43 -0800

    Fixed my code

--- src/main/java/lesser/citibike/lambda/CitiBikeRequestHandler.java
+++ src/main/java/lesser/citibike/lambda/CitiBikeRequestHandler.java
@@ -6,8 +6,6 @@ import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.google.gson.Gson;
 import lesser.citibike.*;
 
-import java.util.Map;
-
 public class CitiBikeRequestHandler implements RequestHandler<APIGatewayProxyRequestEvent, String> {
 
     @Override
@@ -15,36 +13,41 @@ public class CitiBikeRequestHandler implements RequestHandler<APIGatewayProxyReq
         Gson gson = new Gson();
         CitiBikeRequest request = gson.fromJson(event.getBody(), CitiBikeRequest.class);
 
-        try {
-            CitiBikeService service = new CitiBikeServiceFactory().getService();
-            StationsResponse stationsResponse = service.getStations().blockingGet();
-            StatusResponse statusResponse = service.getStationStatus().blockingGet();
-            CitiBikeHelper helper = new CitiBikeHelper(
-                    stationsResponse,
-                    statusResponse
-            );
+        CitiBikeService service = new CitiBikeServiceFactory().getService();
+        StationsResponse stationsResponse = service.getStations().blockingGet();
+        StatusResponse statusResponse = service.getStationStatus().blockingGet();
+        CitiBikeHelper helper = new CitiBikeHelper(
+                stationsResponse,
+                statusResponse
+        );
 
-            StationsResponse.Station startStation = helper.findClosestStationWithBikes(
+        StationsResponse.Station startStation = null;
+        try {
+            startStation = helper.findClosestStationWithBikes(
                     request.from.lat,
                     request.from.lon
             );
-            StationsResponse.Station endStation = helper.findClosestStationWithDocks(
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        StationsResponse.Station endStation = null;
+        try {
+            endStation = helper.findClosestStationWithDocks(
                     request.to.lat,
                     request.to.lon
             );
-
-            CitiBikeResponse response = new CitiBikeResponse(
-                    new Location(request.from.lat, request.from.lon),
-                    new StationLocation(startStation),
-                    new StationLocation(endStation),
-                    new Location(request.to.lat, request.to.lon)
-            );
-
-            return gson.toJson(response);
-
         } catch (Exception e) {
-            context.getLogger().log("Error occurred: " + e.getMessage());
-            return gson.toJson(Map.of("error", e.getMessage()));
+            throw new RuntimeException(e);
         }
+
+        CitiBikeResponse response = new CitiBikeResponse(
+                new Location(request.from.lat, request.from.lon),
+                new StationLocation(startStation),
+                new StationLocation(endStation),
+                new Location(request.to.lat, request.to.lon)
+        );
+
+        return gson.toJson(response);
     }
 }
